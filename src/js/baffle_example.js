96e4b9349737a324658a837318d42155a6c1920c
adarsh-thakur <adarshthakur05@gmail.com>
2019-02-28 11:07:06 +0530

    fixed the upadted outdated packages

--- src/js/baffle_example.js
+++
@@ -1,8 +0,0 @@
-// const baffle = require('baffle');
-const text = window.baffle('.target');
-text.set({
-  characters:'█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░> 🤷‍♂️',
-  speed:120
-})
-text.start()
-text.reveal(4000)
